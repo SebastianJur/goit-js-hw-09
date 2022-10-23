f275e6514d81007069594d147ba4ab71c36027df
Sebastian Jurewicz <senkartan@gmail.com>
2022-10-23 12:02:44 +0200

    Update Promises

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,43 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+const form = document.querySelector('.form');
+const button = document.querySelector('button');
+
+form.addEventListener('submit', handleSubmit);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+        console.log('co to robi?');
+        console.log({ position, delay });
+      } else {
+        // Reject
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+  return promise;
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let amount = Number(form.elements.amount.value);
+  let step = Number(form.elements.step.value);
+  let delay = Number(form.elements.delay.value);
+
+  for (let i = 1; i <= amount; i++) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        console.log('then');
+        console.log({ position, delay });
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌Rejected promise ${position} in ${delay}ms`);
+      });
+
+    delay = step + delay;
   }
 }
